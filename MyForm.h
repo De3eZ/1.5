4bda8b136e0cba155bac0a7697a500c6242a55f3
DeZ <zhilindanilw@gmail.com>
2021-12-17 18:03:15 +0800

    1.5

--- MyForm.h
+++ MyForm.h
@@ -59,7 +59,7 @@ namespace Project1 {
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
 
 	private: System::Windows::Forms::DataGridView^ dataGridView2;
-	private: System::Windows::Forms::Label^ label3;
+
 
 	private: System::ComponentModel::IContainer^ components;
 
@@ -81,7 +81,6 @@ namespace Project1 {
 		{
 			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle1 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
 			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle2 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
@@ -89,20 +88,19 @@ namespace Project1 {
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
-			this->label3 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button3
 			// 
-			this->button3->BackColor = System::Drawing::Color::DarkRed;
+			this->button3->BackColor = System::Drawing::Color::Aqua;
 			this->button3->Font = (gcnew System::Drawing::Font(L"Niagara Solid", 9, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button3->ForeColor = System::Drawing::Color::Black;
-			this->button3->Location = System::Drawing::Point(547, 7);
+			this->button3->Location = System::Drawing::Point(296, 449);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(527, 46);
+			this->button3->Size = System::Drawing::Size(115, 75);
 			this->button3->TabIndex = 3;
 			this->button3->Text = L"Запустить";
 			this->button3->UseVisualStyleBackColor = false;
@@ -110,8 +108,8 @@ namespace Project1 {
 			// 
 			// textBox1
 			// 
-			this->textBox1->BackColor = System::Drawing::Color::DarkRed;
-			this->textBox1->Location = System::Drawing::Point(14, 25);
+			this->textBox1->BackColor = System::Drawing::Color::Aqua;
+			this->textBox1->Location = System::Drawing::Point(12, 452);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(258, 20);
 			this->textBox1->TabIndex = 4;
@@ -119,32 +117,34 @@ namespace Project1 {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(62, 7);
+			this->label1->ForeColor = System::Drawing::Color::Aqua;
+			this->label1->Location = System::Drawing::Point(107, 434);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(140, 15);
+			this->label1->Size = System::Drawing::Size(65, 15);
 			this->label1->TabIndex = 7;
-			this->label1->Text = L"Количество процессов";
+			this->label1->Text = L"Процессы";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(390, 3);
+			this->label2->ForeColor = System::Drawing::Color::Aqua;
+			this->label2->Location = System::Drawing::Point(94, 486);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(34, 15);
+			this->label2->Size = System::Drawing::Size(97, 15);
 			this->label2->TabIndex = 10;
-			this->label2->Text = L"Такт";
+			this->label2->Text = L"Квант времени";
 			// 
 			// textBox2
 			// 
-			this->textBox2->BackColor = System::Drawing::Color::DarkRed;
-			this->textBox2->Location = System::Drawing::Point(282, 25);
+			this->textBox2->BackColor = System::Drawing::Color::Aqua;
+			this->textBox2->Location = System::Drawing::Point(12, 504);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(258, 20);
 			this->textBox2->TabIndex = 9;
 			// 
 			// dataGridView1
 			// 
-			this->dataGridView1->BackgroundColor = System::Drawing::Color::DarkRed;
+			this->dataGridView1->BackgroundColor = System::Drawing::Color::Aqua;
 			dataGridViewCellStyle1->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
 			dataGridViewCellStyle1->BackColor = System::Drawing::Color::White;
 			dataGridViewCellStyle1->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 8.999999F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
@@ -159,45 +159,32 @@ namespace Project1 {
 			dataGridViewCellStyle2->BackColor = System::Drawing::Color::White;
 			dataGridViewCellStyle2->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 8.999999F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			dataGridViewCellStyle2->ForeColor = System::Drawing::Color::DarkRed;
+			dataGridViewCellStyle2->ForeColor = System::Drawing::Color::Black;
 			dataGridViewCellStyle2->SelectionBackColor = System::Drawing::SystemColors::Highlight;
 			dataGridViewCellStyle2->SelectionForeColor = System::Drawing::Color::White;
 			dataGridViewCellStyle2->WrapMode = System::Windows::Forms::DataGridViewTriState::False;
 			this->dataGridView1->DefaultCellStyle = dataGridViewCellStyle2;
 			this->dataGridView1->GridColor = System::Drawing::Color::White;
-			this->dataGridView1->Location = System::Drawing::Point(14, 75);
+			this->dataGridView1->Location = System::Drawing::Point(12, 12);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(519, 331);
+			this->dataGridView1->Size = System::Drawing::Size(404, 198);
 			this->dataGridView1->TabIndex = 11;
 			// 
 			// dataGridView2
 			// 
-			this->dataGridView2->BackgroundColor = System::Drawing::Color::DarkRed;
+			this->dataGridView2->BackgroundColor = System::Drawing::Color::Aqua;
 			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView2->Location = System::Drawing::Point(547, 75);
+			this->dataGridView2->Location = System::Drawing::Point(12, 216);
 			this->dataGridView2->Name = L"dataGridView2";
-			this->dataGridView2->Size = System::Drawing::Size(527, 331);
+			this->dataGridView2->Size = System::Drawing::Size(404, 212);
 			this->dataGridView2->TabIndex = 13;
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(37, 423);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(465, 18);
-			this->label3->TabIndex = 14;
-			this->label3->Text = L"И - исполняется, О - ожидает, Г - готов к иполнению, З - завершён\r\n";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(7, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::WindowText;
-			this->ClientSize = System::Drawing::Size(1088, 459);
-			this->Controls->Add(this->label3);
+			this->BackColor = System::Drawing::Color::White;
+			this->ClientSize = System::Drawing::Size(429, 532);
 			this->Controls->Add(this->dataGridView2);
 			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->label2);
@@ -208,12 +195,11 @@ namespace Project1 {
 			this->Cursor = System::Windows::Forms::Cursors::PanNW;
 			this->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 8.999999F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->ForeColor = System::Drawing::Color::DarkRed;
+			this->ForeColor = System::Drawing::Color::Black;
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
-			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"MyForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"Многоуровневая очередь";
+			this->Text = L"1.5";
 			this->TransparencyKey = System::Drawing::Color::Lime;
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
